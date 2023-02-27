14d53e74d39c3cd01dfd5c6b9c159379e536c5c7
Zia Ahmad <45315917+ziadevcom@users.noreply.github.com>
2023-02-27 17:29:36 +0500

    list steps inside commands.sh file

---
+++ commands.sh
@@ -0,0 +1,9 @@
+# REPO_URL https://github.com/ziadevcom/propeller-github
+1. npx-create-react-app
+2. git commit -m "message" & gh repo create "propeller github" --public.
+3. git checkout -b "update_logo".
+4. Changed the logo inside "/src/App.js" file.
+5. Changes url inside "/src/App.js" file.
+6. git commit -m "updated logo & url"
+7. gh pr create "updated logo".
+8. gh pr merge
